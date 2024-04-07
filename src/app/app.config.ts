3a4edfa44506cba1ba186008257dd7e15920a1f7
rtrydev <r.kulka44@gmail.com>
2024-04-07 14:21:05 +0200

    Scroll to top on navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { InMemoryScrollingOptions, provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptorService } from './auth/auth-interceptor';
 
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top'
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling(scrollConfig)),
     provideHttpClient(withInterceptorsFromDi()),
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
   ]
